147f0c14d1274e81355d2463445cf92bc43a699d
BoBsmil3Y <antoine.dupont59@hotmail.com>
2023-11-05 22:53:19 +0100

    :bug: Fix constructor

--- src/main/java/fr/dupont/models/Thumbnail.java
+++ src/main/java/fr/dupont/models/Thumbnail.java
@@ -2,8 +2,7 @@ package fr.dupont.models;
 
 public class Thumbnail extends Media {
 
-    public Thumbnail(String title, String author, String url, String permaLink, int createdEpochTime, boolean over18, int height, int width, int score, int ups, Float upvoteRatio) {
-        super(title, author, url, permaLink, createdEpochTime, over18, height, width, score, ups, upvoteRatio);
+    public Thumbnail(String title, String author, String url, String permaLink, int createdEpochTime, boolean over18, MediaFormat format, Grade grade) {
+        super(title, author, url, permaLink, createdEpochTime, over18, format, grade);
     }
-
 }
--- src/main/java/fr/dupont/models/Video.java
+++ src/main/java/fr/dupont/models/Video.java
@@ -7,8 +7,8 @@ public class Video extends Media {
 
     private final Float duration;
 
-    public Video(String title, String author, String url, String permaLink, int createdEpochTime, boolean over18, int height, int width, int score, int ups, Float upvoteRatio, Float duration) {
-        super(title, author, url, permaLink, createdEpochTime, over18, height, width, score, ups, upvoteRatio);
+    public Video(String title, String author, String url, String permaLink, int createdEpochTime, boolean over18, MediaFormat format, Grade grade, Float duration) {
+        super(title, author, url, permaLink, createdEpochTime, over18, format, grade);
         this.duration = duration;
     }
 }
