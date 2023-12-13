d6a5512edbb829e76ad8d8892377c98f3a7f231b
Antoine Dupont <antoine.dupont4.etu@univ-lille.fr>
2023-12-13 18:19:42 +0100

    :poop: Reading error stream of process ffmpeg to wait for end of execution. Force to do that to not have an input stream while deleting base audio and video file. waitFor() doesn't work here

--- src/main/java/fr/dupont/videomanipulation/MergeMediaFiles.java
+++ src/main/java/fr/dupont/videomanipulation/MergeMediaFiles.java
@@ -3,17 +3,24 @@ package fr.dupont.videomanipulation;
 import fr.dupont.ColorLogger;
 import fr.dupont.models.Video;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.concurrent.TimeUnit;
+
 public class MergeMediaFiles {
 
-    private ColorLogger logger = new ColorLogger();
+    private final ColorLogger logger = new ColorLogger();
 
     public void mergeAudioAndVideo(Video video) {
 
         final String localUrlMerged = video.getLocalUrl().replace(".mp4", "-merged.mp4");
-        String[] command = {"ffmpeg", "-i", video.getLocalUrl(), "-i", video.getLocalAudioUrl(), "-c:v", "copy", "-c:a", "aac", localUrlMerged};
+        final String[] command = {"ffmpeg", "-i", video.getLocalUrl(), "-i", video.getLocalAudioUrl(), "-c:v", "copy", "-c:a", "aac", "-y", localUrlMerged};
 
         try {
-            new ProcessBuilder(command).start();
+            final Process process = new ProcessBuilder(command).start();
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            while (reader.readLine() != null); //Read error stream to wait for stream close
             logger.print(ColorLogger.Level.SUCCESS, "Merge successful for : " + video.getUrl() + " !");
         } catch (Exception e) {
             logger.print(ColorLogger.Level.ERROR, "An error occurred when merging :  " + video.getUrl() + " ! Error: \n" + e.getMessage() );
@@ -21,7 +28,7 @@ public class MergeMediaFiles {
     }
 
     public boolean isFfmpegIsInstalled() {
-        String[] command = {"ffmpeg", "-version"};
+        final String[] command = {"ffmpeg", "-version"};
         try {
             new ProcessBuilder(command).start();
             logger.print(ColorLogger.Level.INFO, "FFMPEG is installed !");
