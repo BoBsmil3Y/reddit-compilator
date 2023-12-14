a3648465a77da10aa6ef5973d22e66ed00b3d043
BoBsmil3Y <antoine.dupont59@hotmail.com>
2023-12-14 23:48:14 +0100

    :sparkles: Add merge all downloaded videos to create one final

--- src/main/java/fr/dupont/videomanipulation/MergeMediaFiles.java
+++ src/main/java/fr/dupont/videomanipulation/MergeMediaFiles.java
@@ -1,12 +1,13 @@
 package fr.dupont.videomanipulation;
 
 import fr.dupont.ColorLogger;
+import fr.dupont.models.Config;
 import fr.dupont.models.Video;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MergeMediaFiles {
 
@@ -24,6 +25,38 @@ public class MergeMediaFiles {
             logger.print(ColorLogger.Level.SUCCESS, "Merge successful for : " + video.getUrl() + " !");
         } catch (Exception e) {
             logger.print(ColorLogger.Level.ERROR, "An error occurred when merging :  " + video.getUrl() + " ! Error: \n" + e.getMessage() );
+        }
+    }
+
+    public void mergeVideos(Config config, List<Video> videos) {
+
+        final String localUrlMerged = config.video_number() + "-" + config.video_base_name() + ".mp4";
+        final ArrayList<String> command = new ArrayList<>();
+
+        command.add("ffmpeg");
+
+        videos.forEach(video -> {
+            command.add("-i");
+            command.add(video.getLocalUrl());
+        });
+
+        command.add("-filter_complex");
+        command.add("concat=n=" + videos.size() + ":v=1:a=1");
+        command.add("-c:a");
+        command.add("aac");
+        command.add("-y");
+        command.add(localUrlMerged);
+
+        try {
+            logger.print(ColorLogger.Level.LOG, "Starting merge for final video ...");
+            final Process process = new ProcessBuilder(command).start();
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String line;
+            while ((line = reader.readLine()) != null)
+                System.out.println(line); //Read error stream to wait for stream close
+            logger.print(ColorLogger.Level.SUCCESS, "Merge successful! ");
+        } catch (Exception e) {
+            logger.print(ColorLogger.Level.ERROR, "An error occurred when merging the final video!");
         }
     }
 
